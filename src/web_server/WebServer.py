9c9fe767946feba0eb6bb0955b136bf92c92a21a
lehman-china <532031050@qq.com>
2015-05-12 23:55:57 +0800

    初始导入2

--- src/web_server/WebServer.py
+++ src/web_server/WebServer.py
@@ -1,17 +1,26 @@
+import os
+import sys
 from bottle import *
-TEMPLATE_PATH = []
+proPath =  "D:/Users/Desktop/Project/python/ApiDocBrowse"
 
+
+
+
+@route('/include/<filename:path>')
+def send_static(filename):
+    print(filename)
+    return static_file(filename, root=proPath+'/views/include/')
 
 
 # @route('/<filename:path>')
 # def send_static(filename):
-#     return static_file(filename, root='../../views')
+#     return static_file(filename, root=proPath+'/views/include/')
 
 
 @route('/hello/:name')
 def hello(name):
+    return template(proPath + '/views/api.html', username=name)
 
-    return template('/views/hello_template.tpl', username=name)
 
 
 run(host='localhost', port=5000, debug=True)
