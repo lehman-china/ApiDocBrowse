0fdef056c1881690590086e9ca63353948365191
lehman-china <532031050@qq.com>
2015-10-03 22:26:46 +0800

    一个一个的重写以前的js 小工具, json 转java bean工具

--- src/commons/utils/mysql_utils.py
+++ src/commons/utils/mysql_utils.py
@@ -10,14 +10,19 @@ _db_cfg = {
 
 
 # mysql 查询装饰器, qry_type 查询类型默认查询类型
-def query( qry_type = "query" ):
+# 参数sql 有值的话,返回结果集,否则返回cursor 自己操作去!
+def query( qry_type = "query",sql = None):
     def _query( execute ):
         def __query():
             conn = pymysql.connect( host = _db_cfg["host"], port = _db_cfg["port"], user = _db_cfg["user"],
                                     passwd = _db_cfg["passwd"], db = _db_cfg["db"] )
-            cur = conn.cursor( pymysql.cursors.DictCursor )
+            cur = conn.cursor( pymysql.cursors.DictCursor ) # 设置结果集返回 dict 类型
+            if sql :
+                cur.execute( sql )
+                execute( cur.fetchall() )
+            else:
+                execute( cur )
 
-            execute( cur )
             print( "%s sql: %s" % (qry_type,cur._executed) )
             cur.close( )
             conn.close( )
--- src/mytools/vifiadmin_tools/table_entity.py
+++ src/mytools/vifiadmin_tools/table_entity.py
@@ -1,16 +1,68 @@
+import datetime
+from lib.bottle import template
 from src.commons.utils.mysql_utils import init_db,query
-init_db( '192.168.1.212', 'vifiadmin', 'myvifi', 'ViFi' )
+init_db( '120.25.72.53', 'vifiadmin', 'myvifi', 'ViFi' )
 
 
+# aop 注解
+isAopAnnotasion = True
+# spring mvc 传参注解
+isWebAnnotasion = True
+# package_str = "package net.eoutech.commons.entity;";
+package_str = "package com.vifi.vrs.webmin.commons.entity;"
+entityName = "TbUser"
+tabName = "tbUser"
 
-
-
+type_mapping = {
+    "252": "String",
+    "253": "String",
+    "3": "Integer",
+    "8": "Integer",
+    "1": "Integer",
+    "12": "Date"
+}
 
 @query()
 def queryUser( cur ):
-    cur.execute( "SELECT * FROM tbUser LIMIT 0,100" )
-    for user in cur.fetchall( ):
-        print( user )
+    sql= "SELECT * FROM %s limit 0,1" % ( tabName )
+    cur.execute( sql )
+    pk_show = True
+    str = template("""
+      ${package_str}
+      import java.util.Date;
+      
+    % if isWebAnnotasion:
+      import com.alibaba.fastjson.annotation.JSONField;
+      import org.springframework.format.annotation.DateTimeFormat;
+    % end
+
+    % if isAopAnnotasion:
+      import javax.persistence.Id;
+      import javax.persistence.Column;
+      @javax.persistence.Table( name = "${tabName}" )
+    % end
+
+    public class ${entityName} {
+
+    % for desc in cur.description:
+      % if pk_show:
+        @Id
+        <% pk_show=False %>
+      % end
+      % if isAopAnnotasion:
+        @Column( name = "${desc[0]}" )
+      % end
+        private ${type_mapping[str(desc[1])]} ${desc[0]};
+
+    % end
+
+
+
+    }
+    """,dict(globals(), **vars()) )
+
+
+    print( str )
 
     return 0
 
